16f6d61e4a46dc29f5c4740ae4671a4f13b0e61f
alexeyaudi <alexeyaudi@gmail.com>
2025-05-19 16:47:32 +0300

    Update main.rs token

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,49 @@
+//! mine.rs
+
 #[macro_use]
 extern crate rocket;
 
 #[macro_use]
 extern crate log;
 
-use core::fmt;
 use rocket::form::Form;
 use rocket::fs::{FileServer, NamedFile};
 use rocket::http::{ContentType, Method, Status};
-use rocket::request::Request;
+use rocket::request::{FromRequest, Outcome, Request};
 use rocket::response::{self, Responder, Response};
 use rocket_cors::{AllowedOrigins, CorsOptions};
-use std::fmt::{Display, Formatter};
 use std::io::{self, Write};
 use std::path::Path;
 use std::process::{Command, Output, Stdio};
 use tempfile::Builder;
+
+/// Наш «guard» для проверки заголовка Authorization
+pub struct ApiKey(pub String);
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for ApiKey {
+    type Error = ();
+
+    async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        // Собираем все заголовки Authorization
+        let auth_headers: Vec<_> = req.headers().get("Authorization").collect();
+        if auth_headers.len() != 1 {
+            return Outcome::Failure((Status::Unauthorized, ()));
+        }
+        let provided = auth_headers[0];
+
+        // Ожидаемый формат «Bearer <токен>» берём из переменной окружения API_KEY
+        let expected = std::env::var("API_KEY")
+            .map(|v| format!("Bearer {}", v))
+            .unwrap_or_default();
+
+        if provided == expected {
+            Outcome::Success(ApiKey(provided.to_string()))
+        } else {
+            Outcome::Failure((Status::Unauthorized, ()))
+        }
+    }
+}
 
 #[derive(FromFormField)]
 enum PdfEngine {
@@ -24,8 +52,8 @@ enum PdfEngine {
     Pdflatex,
 }
 
-impl Display for PdfEngine {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+impl std::fmt::Display for PdfEngine {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match *self {
             PdfEngine::Weasyprint => write!(f, "weasyprint"),
             PdfEngine::Wkhtmltopdf => write!(f, "wkhtmltopdf"),
@@ -54,94 +82,77 @@ impl From<io::Error> for ConvertError {
 }
 
 impl<'r> Responder<'r, 'static> for ConvertError {
-    fn respond_to(self, _: &Request) -> response::Result<'static> {
+    fn respond_to(self, _: &Request<'_>) -> response::Result<'static> {
         let mut builder = Response::build();
         match self {
-            ConvertError::Output(output) => builder
-                .header(ContentType::Plain)
-                .sized_body(output.stderr.len(), io::Cursor::new(output.stderr))
-                .status(Status::BadRequest),
-            ConvertError::IO(_) => builder.status(Status::InternalServerError),
-        };
-
+            ConvertError::Output(output) => {
+                builder
+                    .header(ContentType::Plain)
+                    .sized_body(output.stderr.len(), io::Cursor::new(output.stderr))
+                    .status(Status::BadRequest);
+            }
+            ConvertError::IO(_) => {
+                builder.status(Status::InternalServerError);
+            }
+        }
         builder.ok()
     }
 }
 
+/// Сам хэндлер конвертации: note, первым аргументом теперь идёт ApiKey
 #[post("/", data = "<form>")]
-async fn convert(form: Form<ConvertForm>) -> Result<NamedFile, ConvertError> {
-    let mut pandoc_builder = Command::new("pandoc");
-
-    // Pandoc can not perform PDF conversion to STDOUT, so we need a temp file
+async fn convert(_key: ApiKey, form: Form<ConvertForm>) -> Result<NamedFile, ConvertError> {
+    // создаём временный PDF-файл
     let pdf_temp_path = Builder::new()
-        // Setting that suffix accomplishes two things:
-        // - Pandoc will know that it should convert to PDF
-        // - Rocket will set the correct Content-Type response header
         .suffix(".pdf")
         .tempfile()
         .map_err(ConvertError::IO)?
         .into_temp_path();
-    let pdf_path = pdf_temp_path
-        .to_str()
-        .expect("Can not deal with non UTF-8 path.");
-    pandoc_builder.arg("--output=".to_owned() + pdf_path);
+    let pdf_path = pdf_temp_path.to_str().unwrap();
 
-    pandoc_builder.arg(
-        "--pdf-engine=".to_owned()
-            + form
-                .engine
-                .as_ref()
-                .unwrap_or(&PdfEngine::Weasyprint)
-                .to_string()
-                .as_str(),
-    );
+    let mut pandoc = Command::new("pandoc");
+    pandoc.arg(format!("--output={}", pdf_path));
+    pandoc.arg(format!(
+        "--pdf-engine={}",
+        form.engine
+            .as_ref()
+            .unwrap_or(&PdfEngine::Weasyprint)
+            .to_string()
+    ));
 
-    // Declare outside of the if block to keep the file around until the end of this function if needed
-    let mut css_file;
-    if form.css.is_some() {
-        css_file = Builder::new()
-            // Necessary for weasyprint to recognize it as a proper stylesheet
+    // опциональный CSS
+    if let Some(css_content) = &form.css {
+        let mut css_file = Builder::new()
             .suffix(".css")
             .tempfile()
             .map_err(ConvertError::IO)?;
-        css_file
-            .write_all(form.css.as_ref().unwrap().as_bytes())
-            .map_err(ConvertError::IO)?;
-        pandoc_builder.arg("--css=".to_owned() + css_file.path().to_str().unwrap());
+        css_file.write_all(css_content.as_bytes()).map_err(ConvertError::IO)?;
+        pandoc.arg(format!("--css={}", css_file.path().display()));
     }
 
-    // We can avoid writing the input to a file by streaming it to STDIN
-    let stdin = Stdio::piped();
-    pandoc_builder.stdin(stdin);
+    // передаём markdown на stdin
+    pandoc.stdin(Stdio::piped());
+    pandoc.stdout(Stdio::piped()).stderr(Stdio::piped());
 
-    pandoc_builder.stdout(Stdio::piped()).stderr(Stdio::piped());
-
-    let mut pandoc_process = pandoc_builder.spawn().map_err(ConvertError::IO)?;
-
-    pandoc_process
+    let mut child = pandoc.spawn().map_err(ConvertError::IO)?;
+    child
         .stdin
         .as_mut()
         .unwrap()
         .write_all(form.markdown.as_bytes())
         .map_err(ConvertError::IO)?;
-
-    let output = pandoc_process
-        .wait_with_output()
-        .map_err(ConvertError::IO)?;
-    debug!("{:?}", output);
+    let output = child.wait_with_output().map_err(ConvertError::IO)?;
 
     if !output.status.success() {
         return Err(ConvertError::Output(output));
     }
 
-    NamedFile::open(Path::new(pdf_path))
-        .await
-        .map_err(ConvertError::IO)
+    NamedFile::open(Path::new(pdf_path)).await.map_err(ConvertError::IO)
 }
 
 #[launch]
 fn rocket() -> _ {
-    // https://github.com/lawliet89/rocket_cors/blob/v0.5.2/examples/fairing.rs
+    // CORS по аналогии с вашим примером
     let cors = CorsOptions::default()
         .allowed_origins(AllowedOrigins::all())
         .allowed_methods(
@@ -150,10 +161,11 @@ fn rocket() -> _ {
                 .map(From::from)
                 .collect(),
         )
-        .to_cors();
+        .to_cors()
+        .expect("CORS config");
 
     rocket::build()
-        .attach(cors.unwrap())
+        .attach(cors)
         .mount("/", routes![convert])
         .mount("/", FileServer::from("static"))
 }
